aaaa5c463851de623ffa6f6073dd967703ac2300
test-dev-mas <yufeng.xu@microartservices.com>
2022-08-22 15:25:34 -0400

    update

--- test.py
+++ test.py
@@ -1,17 +1,45 @@
 import threading
 import time
+import tkinter as tk
+from tkinter import ttk
 
 class A(threading.Thread):
     def __init__(self):
         super().__init__()
 
     def run(self):
-        print("hello world")
-        time.sleep(2)
+        i = 0
+        while True:
+            print("hello world")
+            i += 1
+            if i > 10:
+                break
+            time.sleep(1)
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title('Thread Test')
+        self.geometry('400x200')
+        self.grid_columnconfigure(0,weight=1)
+        self.grid_rowconfigure(0,weight=1)
+
+        self.button = ttk.Button(text='Run', command=self.run)
+        self.button.grid(padx=10,pady=10,column=0, row=0, sticky=tk.NSEW)
+
+    def run(self):
+        a = A()
+        a.start()
+        self.monitor(a)
+
+    def monitor(self, thread):
+        if thread.is_alive():
+            print("alive")
+            self.after(100, lambda: self.monitor(thread))
+        else:
+            print("done")
 
 if __name__ == '__main__':
-    a = A()
-    a.start()
-    print(a.is_alive())         # immediately after thread starts, it should be alive
-    time.sleep(2)
-    print(a.is_alive())         # it should be done by now
+    app = App()
+    app.mainloop()
